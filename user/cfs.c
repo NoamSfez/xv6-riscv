a56487da37bbb602afca8821204150b3c87c3011
NoamSfez <noam.sfez@gmail.com>
2023-04-17 11:55:11 +0000

    add free

--- user/cfs.c
+++ user/cfs.c
@@ -4,95 +4,8 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-char *long_to_str(uint64 num)
-{
-    int isNegative = 0;
-    char *strNum;
-    int i, j, temp;
-    long remainder;
-
-    if (num == 0)
-    {
-        strNum = (char *)malloc(sizeof(char) * 2);
-        strNum[0] = '0';
-        strNum[1] = '\0';
-        return strNum;
-    }
-
-    if (num < 0)
-    {
-        isNegative = 1;
-        num *= -1;
-    }
-
-    remainder = num;
-    i = 0;
-
-    while (remainder != 0)
-    {
-        remainder /= 10;
-        i++;
-    }
-
-    strNum = (char *)malloc(sizeof(char) * (i + isNegative + 1));
-    j = i + isNegative - 1;
-
-    while (num != 0)
-    {
-        temp = num % 10;
-        strNum[j--] = temp + '0';
-        num /= 10;
-    }
-
-    if (isNegative)
-    {
-        strNum[j] = '-';
-    }
-
-    strNum[i + isNegative] = '\0';
-
-    return strNum;
-}
-
-char *make_msg(uint64 pid, uint64 priority, uint64 rtime, uint64 stime, uint64 retime)
-{
-    char *str = (char *)malloc(55 + 5 * sizeof(uint64));
-    char *str1 = "pid: ";
-    char *str2 = " - priority: ";
-    char *str3 = " - rtime: ";
-    char *str4 = " - stime: ";
-    char *str5 = " - retime: ";
-    int offset = 0;
-
-    memcpy(str, str1, strlen(str1));
-    offset += strlen(str1);
-    memcpy(str + offset, long_to_str(pid), strlen(long_to_str(pid)));
-    offset += strlen(long_to_str(pid));
-    memcpy(str + offset, str2, strlen(str2));
-    offset += strlen(str2);
-    memcpy(str + offset, long_to_str(priority), strlen(long_to_str(priority)));
-    offset += strlen(long_to_str(priority));
-    memcpy(str + offset, str3, strlen(str3));
-    offset += strlen(str3);
-    memcpy(str + offset, long_to_str(rtime), strlen(long_to_str(rtime)));
-    offset += strlen(long_to_str(rtime));
-    memcpy(str + offset, str4, strlen(str4));
-    offset += strlen(str4);
-    memcpy(str + offset, long_to_str(stime), strlen(long_to_str(stime)));
-    offset += strlen(long_to_str(stime));
-    memcpy(str + offset, str5, strlen(str5));
-    offset += strlen(str5);
-    memcpy(str + offset, long_to_str(retime), strlen(long_to_str(retime)));
-    offset += strlen(long_to_str(retime));
-    // char *end = (char *)malloc(1);
-    //  memset();
-    memcpy(str + offset, "\0", 1);
-    return str;
-}
-
 void main()
 {
-
     int pid1 = fork();
     if (pid1 == 0)
     {
@@ -114,7 +27,10 @@ void main()
         get_cfs_stats(pid, (unsigned long)priority, (unsigned long)rtime, (unsigned long)stime, (unsigned long)retime);
         printf("pid: %d - priority: %d - rtime: %d - stime: %d - retime: %d\n", pid, *priority, *rtime, *stime, *retime);
 
-        free
+        free(priority);
+        free(rtime);
+        free(stime);
+        free(retime);
     }
     else
     {
@@ -138,6 +54,11 @@ void main()
 
             get_cfs_stats(pid, (unsigned long)priority, (unsigned long)rtime, (unsigned long)stime, (unsigned long)retime);
             printf("pid: %d - priority: %d - rtime: %d - stime: %d - retime: %d\n", pid, *priority, *rtime, *stime, *retime);
+
+            free(priority);
+            free(rtime);
+            free(stime);
+            free(retime);
         }
         else
         {
@@ -161,6 +82,11 @@ void main()
 
                 get_cfs_stats(pid, (unsigned long)priority, (unsigned long)rtime, (unsigned long)stime, (unsigned long)retime);
                 printf("pid: %d - priority: %d - rtime: %d - stime: %d - retime: %d\n", pid, *priority, *rtime, *stime, *retime);
+
+                free(priority);
+                free(rtime);
+                free(stime);
+                free(retime);
             }
             else
             {
